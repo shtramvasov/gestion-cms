1ac51d705fea86d627e1810c63a54bfa2ebd90a8
IrishAir <bls21@mail.ru>
2023-01-09 11:37:25 +0500

    firebase init

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import './firebase'
 import { Provider } from 'react-redux'
 import { store } from '@store/store'
 import '@assets/styles/globals.scss'
