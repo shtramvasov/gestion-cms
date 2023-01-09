cd83dd418e326965159958710f6f5b0401d44b7d
IrishAir <bls21@mail.ru>
2023-01-09 10:36:40 +0500

    Redux store init

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { Provider } from 'react-redux'
+import { store } from '@store/store'
 import '@assets/styles/globals.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<App />,
+	<Provider store={store}>
+		<App />
+	</Provider>,
 )
