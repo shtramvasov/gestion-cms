95e8da1e9561410d5ba0c73e5adcaefd266eed5a
IrishAir <bls21@mail.ru>
2023-01-24 10:59:24 +0500

    lock unlogged users from pressing create task buttons

--- src/pages/DashboardPage/TasksSection/TasksSection.tsx
+++ src/pages/DashboardPage/TasksSection/TasksSection.tsx
@@ -6,9 +6,12 @@ import styles from './TasksSection.module.scss'
 import dayjs from 'dayjs'
 import 'dayjs/locale/ru'
 import { HiPlus } from 'react-icons/hi'
+import { BiLockAlt } from 'react-icons/bi'
 import { useFetchTasksQuery } from '@store/slices/tasksSlice'
+import { useAuth } from '@hooks/useAuth'
 
 const TasksSection: FC = () => {
+	const { isAuth } = useAuth()
 	const { data } = useFetchTasksQuery()
 	const rescentTask = data?.[0]
 	const date = dayjs(rescentTask?.createdAt).locale('ru').format('D MMMM YYYY')
@@ -17,9 +20,15 @@ const TasksSection: FC = () => {
 		<section className={styles.container}>
 			<div className={styles.header}>
 				<Heading text='Последняя задача' date={date} />
-				<Button secondary rounded large>
-					<HiPlus />
-				</Button>
+				{isAuth ? (
+					<Button secondary rounded large>
+						<HiPlus />
+					</Button>
+				) : (
+					<Button secondary rounded large>
+						<BiLockAlt />
+					</Button>
+				)}
 			</div>
 			<TaskItem data={rescentTask} />
 		</section>
