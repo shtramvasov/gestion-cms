3840f5891d397eb5ed6cb16006919d8f6066770a
IrishAir <bls21@mail.ru>
2023-01-30 14:32:20 +0500

    added EmployeeStat component

--- src/pages/EmployeesPage/EmployeesStat/EmployeesStat.tsx
+++ src/pages/EmployeesPage/EmployeesStat/EmployeesStat.tsx
@@ -1,16 +1,34 @@
 import { FC } from 'react'
 import { BiRocket } from 'react-icons/bi'
-import { useFetchUsersQuery } from '@store/slices/usersSlice'
 import StatLayout from '../Statistics/StatLayout/StatLayout'
-import styles from './EmployeesStat.module.scss'
+import { useFetchUsersQuery } from '@store/slices/usersSlice'
 import { reducePositions } from '@utils/reducePositions'
+import styles from './EmployeesStat.module.scss'
 
 const EmployeesStat: FC = () => {
 	const { data: users } = useFetchUsersQuery()
-	const managers = reducePositions(users, 'Менеджер')
+	const data = {
+		managers: reducePositions(users, 'Менеджер'),
+		designers: reducePositions(users, 'Дизайнер'),
+		developers: reducePositions(users, 'Разработчик'),
+		testers: reducePositions(users, 'Тестировщик'),
+	}
 	return (
 		<StatLayout icon={<BiRocket />}>
-			<div className={styles.container}>Stat</div>
+			<ul className={styles.stat}>
+				<li>
+					<span>{data.managers}</span> Менеджеров
+				</li>
+				<li>
+					<span>{data.developers}</span> Разработчиков
+				</li>
+				<li>
+					<span>{data.designers}</span> Дизайнеров
+				</li>
+				<li>
+					<span>{data.testers}</span> Тестировщиков
+				</li>
+			</ul>
 		</StatLayout>
 	)
 }
