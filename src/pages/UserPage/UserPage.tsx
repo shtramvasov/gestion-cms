860ef619ef379b95a2917252e11e127855335bc1
IrishAir <bls21@mail.ru>
2023-01-31 15:02:42 +0500

    pass params to user page

--- src/pages/UserPage/UserPage.tsx
+++ src/pages/UserPage/UserPage.tsx
@@ -1,11 +1,15 @@
 import { FC } from 'react'
+import { useParams } from 'react-router-dom'
 import Layout from '@components/Layout/Layout'
+import { useFetchUserQuery } from '@store/slices/usersSlice'
 import styles from './UserPage.module.scss'
 
 const UserPage: FC = () => {
+	const { id } = useParams()
+	const {data} = useFetchUserQuery(id)
 	return (
 		<Layout>
-			<h1>UserPage</h1>
+			<h1>{data?.name} {data?.position}</h1>
 		</Layout>
 	)
 }
